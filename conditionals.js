653ce08b368f491315c3380702ffd4234019852e
JCPenne <jackcpalmer@gmail.com>
2022-04-21 21:52:19 +0000

    Tinkering with conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,8 +1,8 @@
-const temperature = 12;
+const temperature1 = 12;
 
-if (temperature < 0) {
+if (temperature1 < 0) {
     console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
+} else if (temperature1 < 15) {
     console.log("Short sleeves won't cut it!.");
 } else {
     console.log("Short sleeves are fine.");
@@ -10,16 +10,16 @@ if (temperature < 0) {
 
 console.log("Now you're ready to go outside!");
 
-const isCitizen = true:
+const isCitizen = true;
 const age = 26;
 
 if (isCitizen && age > 18) {
     console.log("You are eligible to vote.");
 }
 
-const temperature = 35
+const temperature2 = 35
 
-if (temperature < -40 || temperature > 40) {
+if (temperature1 < -40 || temperature1 > 40) {
     console.log("Maybe going outside isn't such a great idea.");
 }
 
